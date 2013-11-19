3393b050b499296f1f30b10b1accc7b7cfa4ab9b
Chafic Najjar <chafic.najjar@gmail.com>
2013-11-19 19:45:53 +0200

    fixed bug that did not allow access to videos in nested directories

--- snippet.sh
+++ snippet.sh
@@ -1,9 +1,15 @@
-mkdir ~/Desktop/output
+# Snippet - Chafic Najjar
 
+output_directory="$HOME/Desktop/output"
+
+if [ ! -d "$output_directory" ]; then
+    mkdir ~/Desktop/output
+fi
+
+# current directroy
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-path="$1"
-cd "$path"
+cd "$1"
 
 # find the durations of all mp4 files
 find . -name "*.mp4" |\
@@ -18,21 +24,21 @@ i=0
 array=()
 while read p; do
     array+=("$p")
-    echo ${array[$i]}
     i=$((i+1))
 done < temp
  
 find . -name "*.mp4" > temp2
 
+# trim the videos
 i=0
 while read p; do
-    echo "Reading line $p"
-    echo ${array[$i]}
-    ffmpeg -y -i "$p" -t 10 -ss $((${array[$i]}-10)) -vcodec copy -acodec copy ~/Desktop/output"${p#?}"
+    q=$(echo "$p" | rev | cut -d'/' -f1 | rev)
+    echo "$p"
+    ffmpeg -y -i "$p" -t 10 -ss $((${array[$i]}-10)) -vcodec copy -acodec copy ~/Desktop/output/"$q"
     i=$((i+1))
 done < temp2
 
-cd "$DIR"
-rm "$path/temp"
-rm "$path/temp2"
+# remove temporary files
+rm "temp"
+rm "temp2"
 
