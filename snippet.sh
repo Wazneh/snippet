c1e836b7b3022b4acc46ee11bcab4fca4e4cf247
Chafic Najjar <chafic.najjar@gmail.com>
2013-12-15 19:46:57 +0200

    replaced ffmpeg instructions with avconv instructions

--- snippet.sh
+++ snippet.sh
@@ -14,7 +14,7 @@ cd "$1"
 
 # find the durations of all mp4 files and store in file "durations"
 find . -name "*.mp4" |\
-    xargs -I{} ffmpeg -i {} 2>&1 |\
+    xargs -I{} avprob 2>&1 |\
     grep "Duration" |\
     cut -d ' ' -f 4 |\
     sed s/,// |\
@@ -34,12 +34,11 @@ find . -name "*.mp4" > file_names
 i=0
 while read p; do
     q=$(echo "$p" | rev | cut -d'/' -f1 | rev)
-    echo "$p"
-    ffmpeg -y -i "$p" -t 10 -ss $((${array[$i]}-10)) -vcodec copy -acodec copy ~/Desktop/output/"$q"
+    avconv -ss $((${array[$i]}-9)) -i "$p" -t 10 -vcodec copy -acodec copy ~/Desktop/output/"$q"; 
     i=$((i+1))
 done < file_names 
 
 # remove temporary files
-rm "durations"
-rm "file_names"
+#rm "durations"
+#rm "file_names"
 
